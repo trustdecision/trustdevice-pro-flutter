5f43278a6cba5f922c05f199ac7ce7416cc96283
yang.bai <yang.bai@tongdun.net>
2023-04-14 13:45:43 +0800

    TDFutterRiskUtils adaptation parameter modification

--- android/src/main/java/com/trustdecision/trustdevice_pro_plugin/TDFultterRiskUtils.java
+++ android/src/main/java/com/trustdecision/trustdevice_pro_plugin/TDFultterRiskUtils.java
@@ -5,35 +5,21 @@ import java.util.HashMap;
 import cn.tongdun.mobrisk.TDRisk;
 
 public class TDFultterRiskUtils {
-
-    //必传共有配置
+    ///Mandatory Shared Configuration
     static final String KEY_PARTNER = "partner";
     static final String KEY_APPKEY = "appKey";
     static final String KEY_APPNAME = "appName";
     static final String KEY_COUNTRY = "country";
+    static final String KEY_DEBUG = "debug";
+    static final String KEY_TIME_LIMIT = "timeLimit";
+    static final String KEY_LOCATION = "location";
+    static final String KEY_COLLECT_LEVEL = "collectLevel";
 
-    //可选的共有配置（行为验证配置）
-    static final String KEY_PB_LANGUAGE = "language";
-    static final String KEY_PB_TAPTOCLOSE = "tapToClose";
-    static final String KEY_PB_NEEDSEQID = "needSeqid";
-    static final String KEY_PB_HIDELOADHUB = "hideLoadHud";
-    static final String KEY_PB_HIDEWEBCLOSEBUTTON = "hideWebCloseButton";
-    static final String KEY_PB_OPENLOG = "openLog";
-    static final String KEY_PB_SKIPCAPTCHA = "skipCaptcha";
-    static final String KEY_PB_MFAID = "mfaId";
-
-    //android设备指纹配置
-    static final String KEY_FP_ANDROID_TIMELIMIT = "httpTimeout";
-    static final String KEY_FP_ANDROID_COLLECTLEVEL = "collectLevel";
-    static final String KEY_FP_ANDROID_BLACKBOX_MAXSIZE = "blackBoxMaxSize";
-    static final String KEY_FP_ANDROID_CUSTOM_PROCESSNAME = "customProcessName";
-    static final String KEY_FP_ANDROID_FORCE_TLSVERSION = "forceTLSVersion";
-    static final String KEY_FP_ANDROID_DISABLE_DEBUGGER = "disableDebugger";
-    static final String KEY_FP_ANDROID_DISABLE__RUNNINGTASKS = "disableRunningTasks";
-    static final String KEY_FP_ANDROID_DISABLE__GPS = "disableGPS";
-    static final String KEY_FP_ANDROID_DISABLE__SENSOR = "disableSensor";
-    static final String KEY_FP_ANDROID_DISABLE__READPHONE = "disableReadPhone";
-    static final String KEY_FP_ANDROID_DISABLE__PACKAGELIST = "disableInstallPackageList";
+    ///Android device fingerprint configuration
+    static final String KEY_FP_ANDROID_RUNNING_TASKS = "runningTasks";
+    static final String KEY_FP_ANDROID_SENSOR = "sensor";
+    static final String KEY_FP_ANDROID_READ_PHONE = "readPhone";
+    static final String KEY_FP_ANDROID_INSTALLPACKAGE_LIST = "installPackageList";
 
     /**
      * 转换configMap成builder
@@ -58,70 +44,62 @@ public class TDFultterRiskUtils {
         if (configMap.containsKey(KEY_COUNTRY)) {
             builder.country((String) configMap.get(KEY_COUNTRY));
         }
-        //可选的共有配置（行为验证配置）
-        if (configMap.containsKey(KEY_PB_LANGUAGE)) {
-            builder.language((Integer) configMap.get(KEY_PB_LANGUAGE));
+        if (configMap.containsKey(KEY_DEBUG)) {
+            Object debug = configMap.get(KEY_DEBUG);
+            if (debug != null) {
+                if (!(boolean) debug)
+                    builder.disableDebugger();
+            }
         }
-        if (configMap.containsKey(KEY_PB_TAPTOCLOSE)) {
-            builder.tapToClose((Boolean) configMap.get(KEY_PB_TAPTOCLOSE));
+        if (configMap.containsKey(KEY_TIME_LIMIT)) {
+            Object timeLimit = configMap.get(KEY_TIME_LIMIT);
+            if (timeLimit != null) {
+                builder.httpTimeOut(((int) timeLimit) * 1000);
+            }
         }
-        if (configMap.containsKey(KEY_PB_NEEDSEQID)) {
-            builder.needSeqId((Boolean) configMap.get(KEY_PB_NEEDSEQID));
+        if (configMap.containsKey(KEY_LOCATION)) {
+            Object location = configMap.get(KEY_LOCATION);
+            if (location != null) {
+                if (!(boolean) location)
+                    builder.disableGPS();
+            }
         }
-        if (configMap.containsKey(KEY_PB_HIDELOADHUB)) {
-            builder.hideLoadHud((Boolean) configMap.get(KEY_PB_HIDELOADHUB));
+        if (configMap.containsKey(KEY_COLLECT_LEVEL)) {
+            String collectLevel = (String) configMap.get(KEY_COLLECT_LEVEL);
+            if ("M".equals(collectLevel)) {
+                builder.collectLevel(TDRisk.COLLECT_LEVEL_M);
+            } else if ("L".equals(collectLevel)) {
+                builder.collectLevel(TDRisk.COLLECT_LEVEL_L);
+            }
         }
-        if (configMap.containsKey(KEY_PB_HIDEWEBCLOSEBUTTON)) {
-            builder.hideWebCloseButton((Boolean) configMap.get(KEY_PB_HIDEWEBCLOSEBUTTON));
+        ///Android device fingerprint configuration
+        if (configMap.containsKey(KEY_FP_ANDROID_RUNNING_TASKS)) {
+            Object runningTasks = configMap.get(KEY_FP_ANDROID_RUNNING_TASKS);
+            if (runningTasks != null) {
+                if (!(boolean) runningTasks)
+                    builder.disableRunningTasks();
+            }
         }
-        if (configMap.containsKey(KEY_PB_OPENLOG)) {
-            builder.openLog((Boolean) configMap.get(KEY_PB_OPENLOG));
+        if (configMap.containsKey(KEY_FP_ANDROID_SENSOR)) {
+            Object sensor = configMap.get(KEY_FP_ANDROID_SENSOR);
+            if (sensor != null) {
+                if (!(boolean) sensor)
+                    builder.disableSensor();
+            }
         }
-        if (configMap.containsKey(KEY_PB_SKIPCAPTCHA)) {
-            builder.skipCaptcha((Boolean) configMap.get(KEY_PB_SKIPCAPTCHA));
+        if (configMap.containsKey(KEY_FP_ANDROID_READ_PHONE)) {
+            Object readPhone = configMap.get(KEY_FP_ANDROID_READ_PHONE);
+            if (readPhone != null) {
+                if (!(boolean) readPhone)
+                    builder.disableReadPhone();
+            }
         }
-        if (configMap.containsKey(KEY_PB_MFAID)) {
-            builder.mfaId((String) configMap.get(KEY_PB_MFAID));
-        }
-        //android设备指纹配置
-        if (configMap.containsKey(KEY_FP_ANDROID_TIMELIMIT)) {
-            builder.httpTimeOut((Integer) configMap.get(KEY_FP_ANDROID_TIMELIMIT));
-        }
-        if (configMap.containsKey(KEY_FP_ANDROID_COLLECTLEVEL)) {
-            builder.collectLevel((String) configMap.get(KEY_FP_ANDROID_COLLECTLEVEL));
-        }
-        if (configMap.containsKey(KEY_FP_ANDROID_BLACKBOX_MAXSIZE)) {
-            builder.blackBoxMaxSize((Integer) configMap.get(KEY_FP_ANDROID_BLACKBOX_MAXSIZE));
-        }
-        if (configMap.containsKey(KEY_FP_ANDROID_CUSTOM_PROCESSNAME)) {
-            builder.customProcessName((String) configMap.get(KEY_FP_ANDROID_CUSTOM_PROCESSNAME));
-        }
-        if (configMap.containsKey(KEY_FP_ANDROID_FORCE_TLSVERSION)) {
-            builder.forceTLSVersion((Boolean) configMap.get(KEY_FP_ANDROID_FORCE_TLSVERSION));
-        }
-        if (configMap.containsKey(KEY_FP_ANDROID_DISABLE_DEBUGGER)) {
-            if ((boolean) configMap.get(KEY_FP_ANDROID_DISABLE_DEBUGGER))
-                builder.disableDebugger();
-        }
-        if (configMap.containsKey(KEY_FP_ANDROID_DISABLE__RUNNINGTASKS)) {
-            if ((boolean) configMap.get(KEY_FP_ANDROID_DISABLE__RUNNINGTASKS))
-                builder.disableRunningTasks();
-        }
-        if (configMap.containsKey(KEY_FP_ANDROID_DISABLE__GPS)) {
-            if ((boolean) configMap.get(KEY_FP_ANDROID_DISABLE__GPS))
-                builder.disableGPS();
-        }
-        if (configMap.containsKey(KEY_FP_ANDROID_DISABLE__SENSOR)) {
-            if ((boolean) configMap.get(KEY_FP_ANDROID_DISABLE__SENSOR))
-                builder.disableSensor();
-        }
-        if (configMap.containsKey(KEY_FP_ANDROID_DISABLE__READPHONE)) {
-            if ((boolean) configMap.get(KEY_FP_ANDROID_DISABLE__READPHONE))
-                builder.disableReadPhone();
-        }
-        if (configMap.containsKey(KEY_FP_ANDROID_DISABLE__PACKAGELIST)) {
-            if ((boolean) configMap.get(KEY_FP_ANDROID_DISABLE__PACKAGELIST))
-                builder.disableInstallPackageList();
+        if (configMap.containsKey(KEY_FP_ANDROID_INSTALLPACKAGE_LIST)) {
+            Object installPackageList = configMap.get(KEY_FP_ANDROID_INSTALLPACKAGE_LIST);
+            if (installPackageList != null) {
+                if (!(boolean) installPackageList)
+                    builder.disableInstallPackageList();
+            }
         }
         return builder;
     }
